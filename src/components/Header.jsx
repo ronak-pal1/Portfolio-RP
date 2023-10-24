4e611f4cec7edddb372f2187fe8d25aa8db26f67
Ronak Paul <ronakpaul882@gmail.com>
2023-10-24 20:18:13 +0530

    Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,69 @@
+import { DarkMode, LightMode } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+
+const Header = ({ setDark, currentMode }) => {
+  const changeMode = () => {
+    setDark(!currentMode);
+  };
+
+  return (
+    <header className="flex lg:flex-row items-center lg:justify-evenly justify-center flex-col space-y-4 lg:space-y-0 py-4 px-5 border-b border-gray-200 dark:border-gray-700 lg:sticky lg:top-0 backdrop-blur-lg lg:z-50">
+      <div className="flex flex-row items-center space-x-6">
+        <img
+          src="https://blob.sololearn.com/avatars/fd84fff3-8135-4734-8d94-ec8361704898.jpg"
+          alt="profile"
+          className="lg:w-10 w-7 rounded-full"
+        />
+        <p className="font-bold font-poppins sm:text-xl text-lg dark:text-white">
+          Ronak Paul
+        </p>
+      </div>
+
+      <div className="text-xs sm:text-base space-x-4 font-poppins font-medium dark:text-white items-center">
+        <Link to="/" className=" hover:border-b border-blue-700">
+          Home
+        </Link>
+        <a href="#projects" className=" hover:border-b border-blue-700">
+          Projects
+        </a>
+        <a href="#skills" className=" hover:border-b border-blue-700">
+          Skills
+        </a>
+        <a
+          href="https://codago.hashnode.dev/"
+          target="_blank"
+          className=" hover:border-b border-blue-700"
+        >
+          Blogs
+        </a>
+
+        <a
+          href="https://www.youtube.com/@CodaGo"
+          target="_blank"
+          className=" hover:border-b border-blue-700"
+        >
+          Youtube
+        </a>
+      </div>
+
+      <div className="flex items-center space-x-4">
+        <div
+          onClick={changeMode}
+          className="border border-gray-400 dark:border-white bg-gray-100 dark:bg-black p-1 rounded-full"
+        >
+          {currentMode ? (
+            <LightMode className="text-white" />
+          ) : (
+            <DarkMode className="text-zinc-900" />
+          )}
+        </div>
+
+        <button className="font-poppins bg-blue-500 text-gray-100 py-2 px-3 font-semibold rounded-full sm:text-sm text-xs">
+          Connect
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
