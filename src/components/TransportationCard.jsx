aaa72b76e8fc9055aa98252875061ff3b3a84576
ronak-pal1 <ronakpaul882@gmail.com>
2024-11-02 11:43:02 +0530

    fixed transportation text color and fixed the routing part for SEO

--- src/components/TransportationCard.jsx
+++ src/components/TransportationCard.jsx
@@ -4,7 +4,7 @@ const TransportationCard = ({ title, logo, color }) => {
   return (
     <div className="flex items-center space-x-2 w-fit">
       <GetRequiredLogo logoName={logo} size={30} color={color} />
-      <p className="text-zinc-500 dark:text-gray-300 font-poppins font-bold lg:text-base text-sm">
+      <p className="text-slate-100 font-poppins font-bold lg:text-base text-sm">
         {title}
       </p>
     </div>
